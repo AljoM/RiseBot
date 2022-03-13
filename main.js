29c4dc69759d0af3f5b3f7871599966d2f6a6731
Aljo <aljosa.mesarec@gmail.com>
2022-03-13 16:40:23 +0100

    Fetch all members fix

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { Client, Intents, Collection, MessageEmbed, Role } = require("discord.js");
 const client = new Client({
+  fetchAllMembers: true,
   partials: ["MESSAGE", "CHANNEL", "REACTION", "USER", "GUILD_MEMBER"],
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MEMBERS],
 });
